faa7d6535601102c800a6ebe7f3d35377a1d03eb
EdwinUri <104108960+EdwinUri@users.noreply.github.com>
2022-04-20 17:29:04 -0700

    Pagina aprende a programar

---
+++ index.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards-banner-two', {delay:500});
+ScrollReveal().reveal('.new-carts', {delay:500});
+ScrollReveal().reveal('.cards-banner-one', {delay:500});
